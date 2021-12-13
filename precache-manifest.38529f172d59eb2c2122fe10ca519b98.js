f05da69cf2a275c2c726ef9dda1d907d6540319a
Abhi <abhilash.shoffex@gmail.com>
2021-12-13 18:06:58 +0530

    Buid push testing

---
+++ precache-manifest.38529f172d59eb2c2122fe10ca519b98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e85449e016a458ba02e629ea265da7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f16c3e8f695e75f09fb5",
+    "url": "/static/css/main.24175067.chunk.css"
+  },
+  {
+    "revision": "5307f31df9e5d1f1b210",
+    "url": "/static/js/2.2d373779.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2d373779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f16c3e8f695e75f09fb5",
+    "url": "/static/js/main.6480fcb2.chunk.js"
+  },
+  {
+    "revision": "732f6a7c1c74886a2e16",
+    "url": "/static/js/runtime-main.46a05d98.js"
+  }
+]);
